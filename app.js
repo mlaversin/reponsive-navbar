856ed67840890c615f5caa2fb53efc96bb87dcda
Morgan Laversin <morganlaversin@gmail.com>
2021-07-25 17:09:38 +0200

    burger animation

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
-    // Toggle nav
+    
     burger.addEventListener('click', () => {
-        nav.classList.toogle('nav-active');
+        // Toggle nav
+        nav.classList.toggle('nav-active');
+
+        // Animate links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ''
+            } else {
+                link.style.animation  = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+
+        // Burger animation
+        burger.classList.toggle('toggle');
+
     });
-    // Animate links
-    navLinks.forEach((link, index) => {
-        link.style.animation  = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
-    });
+
 }
 
 
